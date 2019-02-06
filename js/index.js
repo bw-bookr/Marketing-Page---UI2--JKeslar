87e85c3dba71acd245d16d893ac5e4af80412ceb
Jkeslar21 <46494969+Jkeslar21@users.noreply.github.com>
2019-02-06 10:12:24 -0700

    bookrObject created

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+//Bookr Object
+const bookr = [{"genre":"action", "title":"Mockingjay", "author":"Suzanne Collins", "isbn":9783837306910, "rating":4.5},
+{"genre":"comedy", "title":"The Hitchhiker's Guide to the Galaxy", "author":"Douglas Adams", "isbn":9780230529151, "rating":5},
+{"genre":"fantasy", "title":"The Lord of the Flies", "author":"William Golding", "isbn":9784087605785, "rating":4},
+{"genre":"bio", "title":"Scar Tissue", "author":"Anthony Kiedis", "isbn":9788494588655, "rating":5},
+{"genre":"fantasy", "title":"The Hobbit", "author":"J.R.R. Tolkien", "isbn":9789402306538, "rating":4},
+{"genre":"kids", "title":"The Cat In The Hat", "author":"Dr. Seuss", "isbn":9782371000193, "rating":4},
+{"genre":"action", "title":"Into The Wild", "author":"Jon Krakauer", "isbn":9782846664417, "rating":4},
+{"genre":"fantasy", "title":"Perloo The Bold", "author":"Avi", "isbn":9780788736797, "rating":4},
+{"genre":"kids", "title":"The Giving Tree", "author":"Shel Silverstein", "isbn":9789985608289, "rating":4.5},
+{"genre":"comedy", "title":"The Adventures of Captain Underpants", "author":"Dav Pilkey", "isbn":9781537925776, "rating":4.5},
+{"genre":"action", "title":"Robinson Crusoe", "author":"Daniel Defoe", "isbn":9786612175039, "rating":3.5},
+{"genre":"fantasy", "title":"Alice's Adventures in Wonderland", "author":"Lewis Carroll", "isbn":9788862777216, "rating":4},
+{"genre":"kids", "title":"Where The Wild Things Are", "author":"Maurice Sendak", "isbn":9780867642179, "rating":4.5},]
+
 // Tabbed Content
 const tabs = document.querySelector('.tabs');
 const panels = document.querySelectorAll('.panel');
